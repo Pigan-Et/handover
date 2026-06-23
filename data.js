1a6cc8d5d8d2cb790ad58dea42576ed0a8481ec3
Pigan-Et <819287763@qq.com>
2026-06-23 18:48:13 +0800

    Update data.js

--- data.js
+++ data.js
@@ -5,14 +5,12 @@ const tasks = [
 project:"摩配买手",
 owner:"待定",
 email:"xxx@example.com",
-status:"进行中",
 note:"填写交接说明"
 },
 {
 project:"摩配BD",
 owner:"Thinco Zhang",
 email:"thinco.zhang@shopee.com",
-status:"进行中",
 note:"填写交接说明"
 }
 ];
