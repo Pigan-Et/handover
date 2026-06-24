a588e43f18a94ce4525ccdd176f9d3a887d4a2d3
Pigan-Et <819287763@qq.com>
2026-06-24 11:45:34 +0800

    Update data.js
    google sheet test

--- data.js
+++ data.js
@@ -1,16 +1,12 @@
-const leaveDate = "2026-07-15";
-
-const tasks = [
-{
-project:"摩配买手",
-owner:"待定",
-email:"xxx@example.com",
-note:"填写交接说明"
-},
-{
-project:"摩配BD",
-owner:"Thinco Zhang",
-email:"thinco.zhang@shopee.com",
-note:"填写交接说明"
-}
-];
+"const leaveDate = '2026-07-15';
+const tasks = [{project: '摩配买手', owner: 'ZhangMM', email: 'xxx@example.com', note: ''},
+{project: '摩配BD', owner: 'Thinco Zhang', email: 'thinco.zhang@shopee.com', note: ''},
+{project: '汽配BD', owner: 'Cecilia Zhang', email: 'cecilia.zhang01@shopee.com', note: '负责内外配+钥匙扣+其他'},
+{project: '汽配BD', owner: 'Yifeng', email: 'yifeng.gu@shopee.com', note: '负责零件+工具+护理'},
+{project: '重抛货+海运', owner: 'Yana Wang', email: 'yana.wang@shopee.com', note: ''},
+{project: '开PO error 自动化', owner: 'Yara Huang', email: 'yara.huang@shopee.com', note: '目前该自动化已暂停'},
+{project: '核价白名单', owner: 'Joey Chen', email: 'joey.chen@shopee.com', note: ''},
+{project: '退供', owner: 'Betty Wang', email: 'betty.wang01@shopee.com', note: ''},
+{project: 'Ninebot', owner: 'Yifeng', email: 'yifeng.gu@shopee.com', note: ''},
+{project: '来货异常表格更新', owner: 'Icey Guan', email: 'icey.guan@shopeemobile-external.com', note: ''}
+];"
