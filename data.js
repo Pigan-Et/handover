bf78f5382da34e053141fbeb770dd15b5427f96c
Pigan-Et <819287763@qq.com>
2026-06-24 17:15:57 +0800

    Update handover data via G2: 2026/6/24 17:15:57

--- data.js
+++ data.js
@@ -4,8 +4,7 @@ const tasks = [{project: '摩配买手', owner: 'Michelle Zhang', email: 'xxx@ex
 {project: '汽配BD', owner: 'Cecilia Zhang', email: 'cecilia.zhang01@shopee.com', note: '负责内外配+钥匙扣+其他'},
 {project: '汽配BD', owner: 'Yifeng', email: 'yifeng.gu@shopee.com', note: '负责零件+工具+护理'},
 {project: '重抛货+海运', owner: 'Yana Wang', email: 'yana.wang@shopee.com', note: 'https://docs.google.com/spreadsheets/d/1QoNfGS8wEogioxykkflPDkaVAi6EfGswHnEP6KlLJGY/edit?gid=760289418#gid=760289418'},
-{project: '开PO error 自动化', owner: 'Yara Huang', email: 'yara.huang@shopee.com', note: '目前该自动化已暂停
-https://docs.google.com/document/d/1qiTGtd6SP1jeOUK1IbnkyTR27cgNTxVa4LKU3CS5NjU/edit?tab=t.0'},
+{project: '开PO error 自动化', owner: 'Yara Huang', email: 'yara.huang@shopee.com', note: '目前该自动化已暂停https://docs.google.com/document/d/1qiTGtd6SP1jeOUK1IbnkyTR27cgNTxVa4LKU3CS5NjU/edit?tab=t.0'},
 {project: '核价白名单', owner: 'Joey Chen', email: 'joey.chen@shopee.com', note: 'https://docs.google.com/spreadsheets/d/1xXr8eIZqm0E0DI1GebyfxVYBOr_FRFV1OxVo5xu3CeM/edit?gid=423579608#gid=423579608'},
 {project: '退供', owner: 'Betty Wang', email: 'betty.wang01@shopee.com', note: 'https://docs.google.com/spreadsheets/d/1qcNW4hk2wrcY7RQWjEcXqVlzYV2qFxrjHx5_6tn_XzU/edit?gid=855295605#gid=855295605'},
 {project: 'Ninebot', owner: 'Yifeng', email: 'yifeng.gu@shopee.com', note: '-'},
