03b15e9bf6244f3488ba3103ea36b669ff7d0032
Pigan-Et <819287763@qq.com>
2026-06-24 12:10:12 +0800

    Update handover data via G2: 6/24/2026, 12:10:12 PM

--- data.js
+++ data.js
@@ -1,12 +1,13 @@
 const leaveDate = '2026-07-15';
-const tasks = [{project: '摩配买手', owner: 'ZhangMM', email: 'xxx@example.com', note: ''},
-{project: '摩配BD', owner: 'Thinco Zhang', email: 'thinco.zhang@shopee.com', note: ''},
+const tasks = [{project: '摩配买手', owner: 'ZhangMM', email: 'xxx@example.com', note: '-'},
+{project: '摩配BD', owner: 'Thinco Zhang', email: 'thinco.zhang@shopee.com', note: '-'},
 {project: '汽配BD', owner: 'Cecilia Zhang', email: 'cecilia.zhang01@shopee.com', note: '负责内外配+钥匙扣+其他'},
 {project: '汽配BD', owner: 'Yifeng', email: 'yifeng.gu@shopee.com', note: '负责零件+工具+护理'},
-{project: '重抛货+海运', owner: 'Yana Wang', email: 'yana.wang@shopee.com', note: ''},
+{project: '重抛货+海运', owner: 'Yana Wang', email: 'yana.wang@shopee.com', note: 'https://docs.google.com/spreadsheets/d/1QoNfGS8wEogioxykkflPDkaVAi6EfGswHnEP6KlLJGY/edit?gid=760289418#gid=760289418'},
 {project: '开PO error 自动化', owner: 'Yara Huang', email: 'yara.huang@shopee.com', note: '目前该自动化已暂停'},
-{project: '核价白名单', owner: 'Joey Chen', email: 'joey.chen@shopee.com', note: ''},
-{project: '退供', owner: 'Betty Wang', email: 'betty.wang01@shopee.com', note: ''},
-{project: 'Ninebot', owner: 'Yifeng', email: 'yifeng.gu@shopee.com', note: ''},
-{project: '来货异常表格更新', owner: 'Icey Guan', email: 'icey.guan@shopeemobile-external.com', note: ''}
+{project: '核价白名单', owner: 'Joey Chen', email: 'joey.chen@shopee.com', note: 'https://docs.google.com/spreadsheets/d/1xXr8eIZqm0E0DI1GebyfxVYBOr_FRFV1OxVo5xu3CeM/edit?gid=423579608#gid=423579608'},
+{project: '退供', owner: 'Betty Wang', email: 'betty.wang01@shopee.com', note: 'https://docs.google.com/spreadsheets/d/1qcNW4hk2wrcY7RQWjEcXqVlzYV2qFxrjHx5_6tn_XzU/edit?gid=855295605#gid=855295605'},
+{project: 'Ninebot', owner: 'Yifeng', email: 'yifeng.gu@shopee.com', note: '-'},
+{project: '来货异常表格更新', owner: 'Icey Guan', email: 'icey.guan@shopeemobile-external.com', note: '仅负责更新表格公式 & 补充L1 L2数据'},
+{project: '来货异常表格更新', owner: 'Sybil Xu', email: 'sybil.xu@shopee.com', note: '仅负责更新PIC关系'}
 ];
