fc3e7b50345844a9f9ca7216d5bcf9d7f9040013
Pigan-Et <819287763@qq.com>
2026-06-25 15:15:41 +0800

    Update handover data via G2: 2026/6/25 15:15:41

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const leaveDate = '2026-07-15';
 const tasks = [{project: '摩配买手', owner: 'Michelle Zhang', email: 'michelle.zhang@shopee.com', note: 'https://docs.google.com/spreadsheets/d/1K1QMU9KKxGwE3A_rjkR1hE3BgXiUKOmkqJ5lcHdDc5Q/edit?gid=0#gid=0'},
-{project: '摩配BD', owner: 'Thinco Zhang', email: 'thinco.zhang@shopee.com', note: '-'},
+{project: '摩配BD', owner: 'Thinco Zhang', email: 'thinco.zhang@shopee.com', note: '感谢我寒总'},
 {project: '汽配BD', owner: 'Cecilia Zhang', email: 'cecilia.zhang01@shopee.com', note: '负责内外配+钥匙扣+其他'},
 {project: '汽配BD', owner: 'Yifeng', email: 'yifeng.gu@shopee.com', note: '负责零件+工具+护理'},
 {project: '重抛货+海运', owner: 'Yifeng', email: 'yifeng.gu@shopee.com', note: 'https://docs.google.com/spreadsheets/d/1QoNfGS8wEogioxykkflPDkaVAi6EfGswHnEP6KlLJGY/edit?gid=760289418#gid=760289418'},
