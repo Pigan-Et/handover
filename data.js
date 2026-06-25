c4480c8d7d14e5c0f7ef0922118e3b2d71c770b3
Pigan-Et <819287763@qq.com>
2026-06-25 15:14:38 +0800

    Update handover data via G2: 2026/6/25 15:14:38

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 const leaveDate = '2026-07-15';
-const tasks = [{project: '摩配买手', owner: 'Michelle Zhang', email: 'michelle.zhang@shopee.com', note: '-'},
+const tasks = [{project: '摩配买手', owner: 'Michelle Zhang', email: 'michelle.zhang@shopee.com', note: 'https://docs.google.com/spreadsheets/d/1K1QMU9KKxGwE3A_rjkR1hE3BgXiUKOmkqJ5lcHdDc5Q/edit?gid=0#gid=0'},
 {project: '摩配BD', owner: 'Thinco Zhang', email: 'thinco.zhang@shopee.com', note: '-'},
 {project: '汽配BD', owner: 'Cecilia Zhang', email: 'cecilia.zhang01@shopee.com', note: '负责内外配+钥匙扣+其他'},
 {project: '汽配BD', owner: 'Yifeng', email: 'yifeng.gu@shopee.com', note: '负责零件+工具+护理'},
@@ -9,5 +9,6 @@ const tasks = [{project: '摩配买手', owner: 'Michelle Zhang', email: 'michel
 {project: '退供', owner: 'Betty Wang', email: 'betty.wang01@shopee.com', note: 'https://docs.google.com/spreadsheets/d/1qcNW4hk2wrcY7RQWjEcXqVlzYV2qFxrjHx5_6tn_XzU/edit?gid=855295605#gid=855295605'},
 {project: 'Ninebot', owner: 'Yifeng', email: 'yifeng.gu@shopee.com', note: '-'},
 {project: '来货异常表格更新', owner: 'Icey Guan', email: 'icey.guan@shopeemobile-external.com', note: '仅负责更新表格公式 & 补充L1 L2数据'},
-{project: '来货异常表格更新', owner: 'Sybil Xu', email: 'sybil.xu@shopee.com', note: '仅负责更新PIC关系'}
+{project: '来货异常表格更新', owner: 'Sybil Xu', email: 'sybil.xu@shopee.com', note: '仅负责更新PIC关系'},
+{project: '汽摩配SOP', owner: 'Michelle Zhang', email: 'michelle.zhang@shopee.com', note: '业务流程SOP以及日常使用的内部tracker，工具link都在里面了https://docs.google.com/spreadsheets/d/1RJZKJKgr5QnwSC0cTbowMqeDrB8ViLD4i6lVubDzdlw/edit?usp=sharing'}
 ];
