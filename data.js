0d4ea9778ce7d83729439de22f156d0477f2207b
Pigan-Et <819287763@qq.com>
2026-07-08 19:45:02 +0800

    Update handover data via G2: 2026/7/8 19:45:02

--- data.js
+++ data.js
@@ -6,9 +6,9 @@ const tasks = [{project: '摩配买手', owner: 'Michelle Zhang', email: 'michel
 {project: '重抛货+海运', owner: 'Yifeng', email: 'yifeng.gu@shopee.com', note: 'https://docs.google.com/spreadsheets/d/1QoNfGS8wEogioxykkflPDkaVAi6EfGswHnEP6KlLJGY/edit?gid=760289418#gid=760289418'},
 {project: '开PO error 自动化', owner: 'Yara Huang', email: 'yara.huang@shopee.com', note: '目前该自动化已暂停https://docs.google.com/document/d/1qiTGtd6SP1jeOUK1IbnkyTR27cgNTxVa4LKU3CS5NjU/edit?tab=t.0'},
 {project: '核价白名单更新', owner: 'Joey Chen', email: 'joey.chen@shopee.com', note: 'https://docs.google.com/spreadsheets/d/1xXr8eIZqm0E0DI1GebyfxVYBOr_FRFV1OxVo5xu3CeM/edit?gid=423579608#gid=423579608'},
-{project: '白名单（zoe  pic)', owner: '待定', email: '待定', note: '-'},
+{project: '白名单pic', owner: '待定', email: '待定', note: '-'},
 {project: '退供（更新target）', owner: 'Betty Wang', email: 'betty.wang01@shopee.com', note: 'https://docs.google.com/spreadsheets/d/1qcNW4hk2wrcY7RQWjEcXqVlzYV2qFxrjHx5_6tn_XzU/edit?gid=855295605#gid=855295605'},
-{project: '退供（zoe  pic)', owner: '待定', email: '待定', note: '-'},
+{project: '退供pic', owner: '待定', email: '待定', note: '-'},
 {project: 'Ninebot', owner: 'Yifeng', email: 'yifeng.gu@shopee.com', note: '-'},
 {project: '来货异常表格更新', owner: 'Icey Guan', email: 'icey.guan@shopeemobile-external.com', note: '仅负责更新表格公式 & 补充L1 L2数据'},
 {project: '来货异常表格更新', owner: 'Sybil Xu', email: 'sybil.xu@shopee.com', note: 'pic表格自行提取
